6500152174f36b37b2e4883c144b5658d8815cbf
kavencat <shi772425803@163.com>
2022-07-13 21:51:07 +0800

    Merge branch 'android-4.19-stable' 4.19.251 of https://android.googlesource.com/kernel/common Changes in 4.19.252 	esp: limit skb_page_frag_refill use to a single page 	mm/slub: add missing TID updates on slab deactivation 	can: bcm: use call_rcu() instead of costly synchronize_rcu() 	can: grcan: grcan_probe(): remove extra of_node_get() 	can: gs_usb: gs_usb_open/close(): fix memory leak 	usbnet: fix memory leak in error case 	net: rose: fix UAF bug caused by rose_t0timer_expiry 	iommu/vt-d: Fix PCI bus rescan device hot add 	fbcon: Disallow setting font bigger than screen size 	video: of_display_timing.h: include errno.h 	powerpc/powernv: delay rng platform device creation until later in boot 	can: kvaser_usb: replace run-time checks with struct kvaser_usb_driver_info 	can: kvaser_usb: kvaser_usb_leaf: fix CAN clock frequency regression 	can: kvaser_usb: kvaser_usb_leaf: fix bittiming limits 	xfs: remove incorrect ASSERT in xfs_rename 	ARM: meson: Fix refcount leak in meson_smp_prepare_cpus 	pinctrl: sunxi: a83t: Fix NAND function name for some pins 	ARM: at91: pm: use proper compatible for sama5d2's rtc 	ibmvnic: Properly dispose of all skbs during a failover. 	selftests: forwarding: fix flood_unicast_test when h2 supports IFF_UNICAST_FLT 	selftests: forwarding: fix learning_test when h1 supports IFF_UNICAST_FLT 	selftests: forwarding: fix error message in learning_test 	i2c: cadence: Unregister the clk notifier in error path 	misc: rtsx_usb: fix use of dma mapped buffer for usb bulk transfer 	misc: rtsx_usb: use separate command and response buffers 	misc: rtsx_usb: set return value in rsp_buf alloc err path 	ida: don't use BUG_ON() for debugging 	dmaengine: pl330: Fix lockdep warning about non-static key 	dmaengine: at_xdma: handle errors of at_xdmac_alloc_desc() correctly 	dmaengine: ti: Fix refcount leak in ti_dra7_xbar_route_allocate 	dmaengine: ti: Add missing put_device in ti_dra7_xbar_route_allocate 	Linux 4.19.252
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I103a7ac70fedd38208d1d9adc2570adecf38b7db

--- arch/powerpc/platforms/powernv/rng.c
+++ arch/powerpc/platforms/powernv/rng.c
@@ -183,13 +183,8 @@ static int __init pnv_get_random_long_early(unsigned long *v)
 
 	int rc;
 
-	for_each_compatible_node(dn, NULL, "ibm,power-rng") {
-		if (rng_create(dn))
-			continue;
-
-		/* Create devices for hwrng driver */
-		of_platform_device_create(dn, NULL, NULL);
-	}
+	for_each_compatible_node(dn, NULL, "ibm,power-rng")
+		rng_create(dn);
 
 	if (!ppc_md.get_random_seed)
 		return 0;
@@ -213,10 +208,17 @@ void __init pnv_rng_init(void)
 
 static int __init pnv_rng_late_init(void)
 {
+	struct device_node *dn;
 	unsigned long v;
+
 	/* In case it wasn't called during init for some other reason. */
 	if (ppc_md.get_random_seed == pnv_get_random_long_early)
 		pnv_get_random_long_early(&v);
+
+	if (ppc_md.get_random_seed == powernv_get_random_long) {
+		for_each_compatible_node(dn, NULL, "ibm,power-rng")
+			of_platform_device_create(dn, NULL, NULL);
+	}
 }
 machine_subsys_initcall(powernv, pnv_rng_late_init);
 machine_subsys_initcall(powernv, rng_init);
